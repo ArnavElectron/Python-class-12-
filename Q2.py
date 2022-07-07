8e7003dcec78050d8d78ca151e1488842b7cda37
Arnav Rao <90997954+ArnavElectron@users.noreply.github.com>
2022-07-07 11:08:35 +0530

    Create Q2.py

---
+++ Q2.py
@@ -0,0 +1,8 @@
+import pandas as pd
+Resultsheet=pd.DataFrame({'UT1':[57,86,92,52,93,98],'HalfYearly':[83,67,78,84,75,79],'UT2':[49,87,45,55,87,88],'Annual':[89,90,66,78,69,96]},index=["Shara","Mansi","Kanika","Ramesh","Ankita","Pranay"])
+
+Resultsheet.index=[1,2,3,4,5,6]
+
+Resultsheet.rename(columns={"Term1","Term2","Term3","Term4"})
+print(Resultsheet)
+
