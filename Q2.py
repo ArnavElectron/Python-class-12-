5e20bbe20c1739663d697240c7744c9d80b4c04b
Arnav Rao <90997954+ArnavElectron@users.noreply.github.com>
2022-07-07 11:12:37 +0530

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,8 +1,8 @@
 import pandas as pd
 Resultsheet=pd.DataFrame({'UT1':[57,86,92,52,93,98],'HalfYearly':[83,67,78,84,75,79],'UT2':[49,87,45,55,87,88],'Annual':[89,90,66,78,69,96]},index=["Shara","Mansi","Kanika","Ramesh","Ankita","Pranay"])
-
+#1
 Resultsheet.index=[1,2,3,4,5,6]
-
-Resultsheet.rename(columns={"Term1","Term2","Term3","Term4"})
+#2
+Resultsheet.rename(columns={"UT1":"Term1","HalfYearly":"Term2","UT2":"Term3","Annual":"Term4"},inplace=True)
 print(Resultsheet)
 
